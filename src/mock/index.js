1a542731107dd798d96cdbe94e854df5e1612648
songchao <songchao_ss@163.com>
2020-06-17 18:29:43 +0800

    部门前段

--- src/mock/index.js
+++ src/mock/index.js
@@ -11,6 +11,6 @@ Mock.setup({
 Mock.mock(/\/table\/list\.*/, 'get', TableAPI.list)
 Mock.mock(/\/frame\/profile/, 'post', ProfileAPI.profile)
 Mock.mock(/\/frame\/login/, 'post', LoginAPI.login)
-Mock.mock(/\/company\/+/, 'get', companyAPI.sassDetail)
-Mock.mock(/\/company/, 'get', companyAPI.list)
+// Mock.mock(/\/company\/+/, 'get', companyAPI.sassDetail)
+// Mock.mock(/\/company/, 'get', companyAPI.list)
 
