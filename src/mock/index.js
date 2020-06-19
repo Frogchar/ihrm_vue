30c55bb6010da2e7ff8d185e2d876e825af504d5
songchao <songchao_ss@163.com>
2020-06-19 15:24:01 +0800

    finish show page

--- src/mock/index.js
+++ src/mock/index.js
@@ -9,8 +9,8 @@ Mock.setup({
 })
 
 Mock.mock(/\/table\/list\.*/, 'get', TableAPI.list)
-Mock.mock(/\/frame\/profile/, 'post', ProfileAPI.profile)
-Mock.mock(/\/frame\/login/, 'post', LoginAPI.login)
+// Mock.mock(/\/frame\/profile/, 'post', ProfileAPI.profile)
+// Mock.mock(/\/frame\/login/, 'post', LoginAPI.login)
 // Mock.mock(/\/company\/+/, 'get', companyAPI.sassDetail)
 // Mock.mock(/\/company/, 'get', companyAPI.list)
 
